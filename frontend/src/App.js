d01f29cf50b84c2c2398685b2da7087f6e7c6ca2
Gaurav <gauravsarage.1902@gmail.com>
2022-09-20 14:32:38 +0530

    HomeScreen Product Listing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,26 @@
 import React from 'react';
+import { Container } from 'react-bootstrap'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import HomeScreen from './screens/HomeScreen';
 
 const App = () => {
   return (
     <>
-      <h1>Welcome to Shopping App</h1>
+
+    <Header />
+
+    <main className='py-3'>
+      <Container>
+        <HomeScreen />
+          <h1>Welcome to Shopping App</h1>
+      </Container>
+    </main>
+
+    <Footer />
     </>
   );
 }
 
 export default App;
+ 
